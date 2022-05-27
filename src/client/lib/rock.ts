141f8eb8f8fec09fd29ff43062b19aa29ea5522b
3a1b2c3 <kabischmid@gmail.com>
2022-05-27 23:18:37 +1000

    rock

---
+++ src/client/lib/rock.ts
@@ -0,0 +1,31 @@
+import * as THREE from 'three'; 
+
+import { StoneDefault, reset } from "./skipping";
+import { RockState, RockHandling} from '../types/types'
+import { defaultPositionY, defaultRoationX } from "./meshes";
+import { removeEntity } from "./Scene";
+import { setText } from "./headsUp";
+
+const defaultLabelFont = 13;
+
+export const rockHandling : RockHandling = {
+  rockState: RockState.start,
+  rockMeshes: Array<THREE.Mesh>(),
+  intersections : null,
+  stoneSimulation : Object.create(StoneDefault)
+};
+
+
+export function resetRock(Scene : any, rockHandling :any){
+  reset(rockHandling.stoneSimulation);
+  rockHandling.rockState = RockState.start;
+  if (Scene){
+    removeEntity(defaultLabel, Scene);
+    setText(rockHandling.rockState, rockHandling.stoneSimulation,
+       defaultLabel, defaultLabelFont);
+  }
+  if (rockHandling.rockMeshes && rockHandling.rockMeshes[0]){
+    rockHandling.rockMeshes[0].position.set(0, defaultPositionY, 0);
+    rockHandling.rockMeshes[0].rotateX(defaultRoationX);
+  }
+}
